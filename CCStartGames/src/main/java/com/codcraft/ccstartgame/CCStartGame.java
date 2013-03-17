2af4175ab19fe8e6f27e49e94f7d8f9327266008
joby890 <mcjoby890@gmail.com>
2013-03-17 16:18:22 -0700

    Mainly Clean up of CCStartGames

--- CCStartGames/src/main/java/com/codcraft/ccstartgame/CCStartGame.java
+++ CCStartGames/src/main/java/com/codcraft/ccstartgame/CCStartGame.java
@@ -1,34 +1,15 @@
 package com.codcraft.ccstartgame;
-
 import java.io.File;
-
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import com.codcraft.ccommands.CCCommands;
-
 public class CCStartGame extends JavaPlugin {
-	private CCCommands ccommands;
 	public void onEnable(){
-		final Plugin cccommands = Bukkit.getPluginManager().getPlugin("CCCommands");
-		if(cccommands == null) {
-			getServer().getPluginManager().disablePlugin(this);
-		}
-		this.ccommands = (CCCommands) cccommands;
-		LoadMaps();
-	}
-
-	private void LoadMaps() {
-		if(!getDataFolder().exists()) {
-			getDataFolder().mkdir();
-		}
-		YamlConfiguration config = new YamlConfiguration().loadConfiguration(new File(getDataFolder(), "config.yml"));
+		if(!getDataFolder().exists()) getDataFolder().mkdir();	
+		new YamlConfiguration();
+		YamlConfiguration config = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "config.yml"));
 		for(String s : config.getConfigurationSection("games").getKeys(false)) {
 			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "a startgame "+ config.getString("games."+s+".game") +" " + s);
 		}
-		
 	}
-	
 }
