56aba424680f98f0d8f7dac8523535455b4c5520
joby890 <mcjoby890@gmail.com>
2013-10-01 11:11:40 -0400

    Commit out the join leave messages.

--- CCBunngie/src/main/java/com/codcraft/bunggie/Listener.java
+++ CCBunngie/src/main/java/com/codcraft/bunggie/Listener.java
@@ -1,12 +1,7 @@
 package com.codcraft.bunggie;
 
 
-import java.util.TimerTask;
-
-import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.PostLoginEvent;
@@ -14,6 +9,7 @@ import net.md_5.bungee.event.EventHandler;
 
 public class Listener implements net.md_5.bungee.api.plugin.Listener {
 
+	@SuppressWarnings("unused")
 	private Main plugin;
 
 	public Listener(Main plugin) {
@@ -21,36 +17,38 @@ public class Listener implements net.md_5.bungee.api.plugin.Listener {
 	}
 
 	
+	
 	@EventHandler
 	public void onJoin(LoginEvent e) {
+		
 		if(e.isCancelled()) {
 			e.setCancelReason(ChatColor.RED+"Updating the Lobby. Please reconnect in a few.");
 		}
 	}
 	@EventHandler
 	public void postloginevent(final PostLoginEvent e) {
-		plugin.timer.schedule(new TimerTask() {
+		/*plugin.timer.schedule(new TimerTask() {
 			
 			@Override
 			public void run() {
 				for(ProxiedPlayer pp: BungeeCord.getInstance().getPlayers()){
-					pp.sendMessage(ChatColor.YELLOW + e.getPlayer().getName() + " has joined CodCraftNetwork");
+					pp.sendMessage(ChatColor.YELLOW + e.getPlayer().getName() + " has joined CylumNetwork");
 				}
 			}
-		}, 1);
+		}, 1);*/
 	}
 	
 	@EventHandler
 	public void onLeave(final PlayerDisconnectEvent e) {
-		plugin.timer.schedule(new TimerTask() {
+		/*plugin.timer.schedule(new TimerTask() {
 			
 			@Override
 			public void run() {
 				for(ProxiedPlayer p : ProxyServer.getInstance().getPlayers()) {
-					p.sendMessage(ChatColor.YELLOW + "" + e.getPlayer().getName() + " has left CodCraftNetwork!");
+					p.sendMessage(ChatColor.YELLOW + "" + e.getPlayer().getName() + " has left CylumNetwork!");
 				}
 			}
-		}, 1);
+		}, 1);*/
 
 	}
 }
