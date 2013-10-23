25af7dcc662953efc2ac308d836d69c952adde9c
lyokofirelyte <lyokofirelyte@gmail.com>
2013-10-23 17:41:07 -0500

    Moving Fling Plates

--- BattleGame/src/com/admixhosting/battleroom/listener/GameListener.java
+++ BattleGame/src/com/admixhosting/battleroom/listener/GameListener.java
@@ -424,7 +424,6 @@ public class GameListener implements Listener {
 
 	}
 
-	
 	@EventHandler
 	public void onInt(PlayerInteractEvent e) {
 		GameManager gm = plugin.api.getModuleForClass(GameManager.class);
@@ -437,12 +436,6 @@ public class GameListener implements Listener {
 					}
 				}
 
-			}
-		} else {
-			if (e.getAction() == Action.PHYSICAL && e.getPlayer().getLocation().getBlock().getType().equals(Material.STONE_PLATE)){
-				Vector vec = e.getPlayer().getEyeLocation().getDirection();
-			    e.getPlayer().setVelocity(vec.multiply(4));
-			    e.getPlayer().getWorld().playSound(e.getPlayer().getLocation(), Sound.GHAST_FIREBALL, 3.0F, 0.5F);
 			}
 		}
 
