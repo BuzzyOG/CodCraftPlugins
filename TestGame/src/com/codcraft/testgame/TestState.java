adc14a0d576e7cb045c23ebf408f74f7d8578305
joby890 <mcjoby890@gmail.com>
2014-01-01 22:11:24 -0500

    Added defaults for TestState

--- TestGame/src/com/codcraft/testgame/TestState.java
+++ TestGame/src/com/codcraft/testgame/TestState.java
@@ -1,56 +1,73 @@
 package com.codcraft.testgame;
 
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
+
 import com.CodCraft.api.model.Game;
 import com.CodCraft.api.model.GameState;
 
 public class TestState implements GameState {
 
+	private TestGame plugin;
+	private int duration;
+	private Game<?> game;
+	private BukkitTask task;
+
+	public TestState(TestGame plugin) {
+		this.plugin = plugin;
+	}
+
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
+		return "TestState";
 	}
 
 	@Override
 	public void setTimeLeft(int duration) {
-		// TODO Auto-generated method stub
+		this.duration = duration;
 		
 	}
 
 	@Override
 	public int getTimeLeft() {
-		// TODO Auto-generated method stub
-		return 0;
+		return duration;
 	}
 
 	@Override
 	public Runnable getTask() {
-		// TODO Auto-generated method stub
-		return null;
+		Runnable run = new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		return run;
 	}
 
 	@Override
 	public void setGame(Game<?> game) {
-		// TODO Auto-generated method stub
+		this.game = game;
 		
 	}
 
 	@Override
 	public Game<?> getGame() {
-		// TODO Auto-generated method stub
-		return null;
+		return game;
 	}
 
 	@Override
 	public void Start() {
-		// TODO Auto-generated method stub
-		
+		duration = 60;
+		task = Bukkit.getScheduler().runTaskTimer(getGame().getPlugin(), getTask(), 0, 20);
 	}
 
 	@Override
 	public void Stop() {
-		// TODO Auto-generated method stub
-		
+		if(task != null) {
+			task.cancel();
+		}
 	}
 
 }
