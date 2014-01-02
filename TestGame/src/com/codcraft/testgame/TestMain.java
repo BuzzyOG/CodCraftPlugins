ddb083dc66719155abc1c184baf61481f29ed818
joby890 <mcjoby890@gmail.com>
2014-01-01 22:01:47 -0500

    Added defaults to TestMain

--- TestGame/src/com/codcraft/testgame/TestMain.java
+++ TestGame/src/com/codcraft/testgame/TestMain.java
@@ -1,22 +1,24 @@
 package com.codcraft.testgame;
 
+import com.CodCraft.api.modules.GameManager;
 import com.CodCraft.api.services.CCGamePlugin;
 
 public class TestMain extends CCGamePlugin {
 	
 	public void onEnable() {
-		
+		getServer().getPluginManager().registerEvents(new TestListener(), this);
 	}
 
 	@Override
 	public String getTag() {
-		// TODO Auto-generated method stub
-		return null;
+		return "TestGame";
 	}
 
 	@Override
 	public void makeGame(String[] args) {
-		// TODO Auto-generated method stub
+		TestGame game = new TestGame(this);
+		game.setName(args[0]);
+		getApi().getModuleForClass(GameManager.class).registerGame(game);
 		
 	}
 
