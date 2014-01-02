8e86f4113afa95c26ad30c7a81ffcc7b1564951d
joby890 <mcjoby890@gmail.com>
2014-01-01 22:11:52 -0500

    Added defaults for TestListeners

--- TestGame/src/com/codcraft/testgame/TestListener.java
+++ TestGame/src/com/codcraft/testgame/TestListener.java
@@ -3,5 +3,11 @@ package com.codcraft.testgame;
 import org.bukkit.event.Listener;
 
 public class TestListener implements Listener {
+	
+	private TestMain plugin;
+
+	public TestListener(TestMain plugin) {
+		this.plugin = plugin;
+	}
 
 }
